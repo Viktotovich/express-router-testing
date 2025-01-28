0983f2d1de66eb386f62075c95121ed437bf645f
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-01-29 02:25:51 +0400

    Implemented :ids in routing, tested extracting params and queries for middleware

--- server.js
+++ server.js
@@ -6,6 +6,23 @@ const PORT = process.env.PORT;
 
 app.get("/", (req, res) => res.send("Hello, world!"));
 
+app.post("/messages", (req, res) =>
+  res.send("This is where you can see any messages.")
+);
+
+app.get("/:username/messages", (req, res) => {
+  console.log("Params:", req.params);
+  console.log("Query:", req.query);
+  res.end();
+});
+
+app.get("/:username/messages/:messageId", (req, res) => {
+  console.log(req.params);
+  res.end();
+});
+
+app.get("*", (req, res) => res.send("404, Page not Found"));
+
 app.listen(PORT, () => {
   console.log(`Works well, listening on port ${PORT}`);
 });
