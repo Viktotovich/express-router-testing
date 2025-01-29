729d8e740fc45bcabbe43fe178985d18e34fec64
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-01-29 17:44:05 +0400

    Added error handlers and an intentional route that always throws

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 app.use("/", indexRouter);
 
+app.get("/throw", (req, res, next) => {
+  throw new Error("Boom");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("Not Found");
+});
+
 app.listen(PORT, () => {
-  console.log(`Please work. We're live on ${PORT}`);
+  console.log(`We're live on ${PORT}`);
+});
+
+/* This is supposed to be a "catch-all" middleware */
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err);
 });
